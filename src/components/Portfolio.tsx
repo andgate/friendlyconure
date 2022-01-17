5afedb8eaf89c48de81d247d61ca495d132c95b9
Gabriel Anderson <gabe4k@gmail.com>
2022-01-16 21:49:22 -0800

    fixed issue where left and right margin of portfolio component was too large

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -1,19 +1,18 @@
 import { style } from 'typestyle'
 import * as csstips from 'csstips'
-import { percent, px } from 'csx'
+import { px } from 'csx'
 import { portfolioImages } from '../assets/images/portfolio'
 import { useState } from 'react'
 import { ImageModal } from './ImageModal'
 
-const portfolioRoot = style(csstips.vertical, csstips.center)
-
-const portfolioListContainer = style(
+const portfolioRoot = style(
   csstips.horizontal,
   csstips.centerJustified,
   csstips.wrap,
-  csstips.width(percent(80)),
   {
-    marginTop: px(5)
+    marginTop: px(5),
+    marginLeft: px(25),
+    marginRight: px(25)
   }
 )
 
@@ -51,17 +50,15 @@ export function Portfolio() {
 
   return (
     <div id="portfolio-root" className={portfolioRoot}>
-      <div id="portfolio-list-container" className={portfolioListContainer}>
-        {portfolioImages.map((imgUrl, i) => (
-          // Uses index as key, since portfolioImages is ultimately static
-          <img
-            key={i}
-            onClick={() => openModal(imgUrl)}
-            src={imgUrl}
-            className={portfolioItem}></img>
-        ))}
-        <ImageModal onClick={closeModal} imgSrc={modalImgSrc}></ImageModal>
-      </div>
+      {portfolioImages.map((imgUrl, i) => (
+        // Uses index as key, since portfolioImages is ultimately static
+        <img
+          key={i}
+          onClick={() => openModal(imgUrl)}
+          src={imgUrl}
+          className={portfolioItem}></img>
+      ))}
+      <ImageModal onClick={closeModal} imgSrc={modalImgSrc}></ImageModal>
     </div>
   )
 }
