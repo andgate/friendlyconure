b48931f7a232830936a5571cf4a6e51e5c1b9961
Gabriel Anderson <gabe4k@gmail.com>
2022-03-17 07:41:12 -0700

    added keepportlandconnect colored variant to portfolio

--- src/assets/images/portfolio/index.ts
+++ src/assets/images/portfolio/index.ts
@@ -28,7 +28,8 @@ import picUrl_27 from './moss1.jpg'
 import picUrl_28 from './moss2.jpg'
 import picUrl_29 from './student-portrait.jpg'
 import picUrl_30 from './u_can_do_it_bro_i_believe_in_you.jpg'
-import picUrl_31 from './keepportlandconnected2.png'
+import picUrl_31 from './keepportlandconnected1.png'
+import picUrl_32 from './keepportlandconnected2.png'
 
 // List of portfolio images, sorted by oldest.
 export const portfolioImages = [
@@ -62,5 +63,6 @@ export const portfolioImages = [
   picUrl_28,
   picUrl_29,
   picUrl_30,
-  picUrl_31
+  picUrl_31,
+  picUrl_32
 ].reverse() // reverse to sort by most recent
